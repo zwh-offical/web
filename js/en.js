f8e6bddc764a469db8df69fdf40fbe3df5a6112a
zwh-offical <140790953+zwh-offical@users.noreply.github.com>
2024-10-04 11:50:41 +0800

    Add files via upload

---
+++ js/en.js
@@ -0,0 +1,16 @@
+document.getElementById('content').innerHTML = `
+    <h1>Weihao's Website</h1>
+    <p><br></p>
+    <p>email:<a href="mailto:zwh@outlook.jp" class="__cf_email__" data-cfemail="5e2429361e2429366f6c6d703d30">zwh@outlook.jp</a><br></p>
+    <p>QQ: 3368151505<br></p>
+    <p><br></p>
+    <div style>
+        <span><span><strong><span><span><span style="color:#E53333;font-size:18px;"><span><span>Please bookmark this website, it is permanently valid</span></span></span></span></span></strong></span></span><span style="color:#E53333;font-size:18px;"><strong></strong></span>
+    </div>
+`;
+
+document.getElementById('nav-links').innerHTML = `
+    <li><a href="https://home.zwh123.cn">Introduction</a></li>
+    <li><a href="https://blog.zwh123.cn">Blog</a></li>
+    <li><a href="https://zwh233.top" target="_blank">Backup Site</a></li>
+`;
---
+++ js/ja.js
@@ -0,0 +1,16 @@
+document.getElementById('content').innerHTML = `
+    <h1>炜皓のウェブサイト</h1>
+    <p><br></p>
+    <p>連絡先メール：<a href="mailto:zwh@outlook.jp" class="__cf_email__" data-cfemail="5e2429361e2429366f6c6d703d30">zwh@outlook.jp</a><br></p>
+    <p>QQ: 3368151505<br></p>
+    <p><br></p>
+    <div style>
+        <span><span><strong><span><span><span style="color:#E53333;font-size:18px;"><span><span>このウェブサイトをブックマークしてください。永久に有効です</span></span></span></span></span></strong></span></span><span style="color:#E53333;font-size:18px;"><strong></strong></span>
+    </div>
+`;
+
+document.getElementById('nav-links').innerHTML = `
+    <li><a href="https://home.zwh123.cn">紹介</a></li>
+    <li><a href="https://blog.zwh123.cn">ブログ</a></li>
+    <li><a href="https://zwh233.top" target="_blank">バックアップサイト</a></li>
+`;
