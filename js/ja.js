4f69b48249ea6a5622bc45b96e373652813c35ea
zwh-offical <140790953+zwh-offical@users.noreply.github.com>
2025-02-14 11:39:11 +0800

    Update ja.js

--- js/ja.js
+++ js/ja.js
@@ -2,7 +2,6 @@ document.getElementById('content').innerHTML = `
     <h1>炜皓のウェブサイト</h1>
     <p><br></p>
     <p>連絡先メール：<a href="mailto:zwh@outlook.jp" class="__cf_email__" data-cfemail="5e2429361e2429366f6c6d703d30">zwh@outlook.jp</a><br></p>
-    <p>QQ: 3368151505<br></p>
     <p><br></p>
     <div style>
         <span><span><strong><span><span><span style="color:#E53333;font-size:18px;"><span><span>このウェブサイトをブックマークしてください。永久に有効です</span></span></span></span></span></strong></span></span><span style="color:#E53333;font-size:18px;"><strong></strong></span>
